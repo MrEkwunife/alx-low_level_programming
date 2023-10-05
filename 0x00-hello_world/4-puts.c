9901090cbc096154d23efdef4e00a082d3f823b4
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-05 09:23:48 -0700

    error corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* The main function prints a text */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
