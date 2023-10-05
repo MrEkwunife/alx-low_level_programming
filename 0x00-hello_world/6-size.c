aeb2f2606108b9c6c373908601bec8fe050f2d06
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-05 12:58:17 -0700

    printing sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of data types
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char aCharacter;
+	int integer;
+	long int aLongInteger;
+	long long int aLongerInteger;
+	float aFloat;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(aCharacter));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(aLongInteger));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(aLongerInteger));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(aFloat));
+
+	return (0);
+}
