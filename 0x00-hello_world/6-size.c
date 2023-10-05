bfff1274afdf74ba87f6a58a7572f1e80411286c
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-05 13:00:54 -0700

    error resolved

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(aCharacter));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(aLongInteger));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(aLongerInteger));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(aLongerInteger));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(aFloat));
 
 	return (0);
