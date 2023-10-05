d0a00d694cc3805174fe810a6de420093c48b7fa
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-05 09:35:19 -0700

    betty 😐😐

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* The main function prints a text */
+/**
+ * main - Prints a sentence to screen
+ * Return: returns void or zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
