cc890d176a3dd8335751f1b2df039e93a0f4ec7a
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-05 13:13:39 -0700

    betty again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	char aCharacter;
 	int integer;
-	long int aLongInteger;
-	long long int aLongerInteger;
+	long int aLI;
+	long long int aLLI;
 	float aFloat;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(aCharacter));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(aLongInteger));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(aLongerInteger));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(aLI));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(aLLI));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(aFloat));
 
 	return (0);
