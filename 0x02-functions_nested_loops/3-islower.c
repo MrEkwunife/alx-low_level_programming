123b91baa9119ed27fb57f24565e26f0a0775a2d
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-10 15:00:23 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - returns a value
+ *
+ * Return: 1 if c is lower, else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
