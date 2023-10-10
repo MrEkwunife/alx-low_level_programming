c97a215e58553fc85aa3a266e46e5c6591b88046
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-10 10:47:05 -0700

    alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * main -prints lowercase alphabets
+ *
+ * Return: Nill
+ */
+
+void print_alphabet(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+	_putchar('\n');
+}
