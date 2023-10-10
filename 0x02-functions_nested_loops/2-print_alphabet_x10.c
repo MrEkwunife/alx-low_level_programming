73cfe9e4cd36766a99c1efd371db8e0d6d7f8e24
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-10 13:33:34 -0700

    printing alphbets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the engkish alphabets ten time in new lines
+ *
+ * Return: on success, 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char al = 'a';
+
+	while (num <= 9)
+	{
+		while (al <= 'z')
+			_putchar(al);
+		_putchar('\n');
+		num++;
+	}
+}
