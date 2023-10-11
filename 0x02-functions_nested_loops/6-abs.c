110e394a7aaf8cfbebd85cec042feb0571f93228
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-10 20:17:26 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The integer to be examined
+ * Return: The magnitude of an intege
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
