70c96d3a2bfdbe430de371dc95672b3655012ab3
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-10 21:03:04 -0700

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * add - adds two integers 
+  * @a: first int 
+  * @b: second int
+  * Return: a + b
+  */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
