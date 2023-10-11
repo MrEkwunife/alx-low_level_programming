95bea203bb6775e0bb1120208c097bd22db8ccc6
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-10 19:57:07 -0700

    prints aigns before numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * This code displays the sign of a number
+ * @n: The integer to be examined.
+ * Return: The code displays a "+" if 'n' is greater than zero
+ * The code outputs "0" if 'n' equals zero.
+ * This code prints a "-" if 'n' is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
+
+
