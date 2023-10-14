51cddfdd34afa2e7aaaca976c1ea90c03ad8348f
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-14 14:34:45 +0000

    corrected

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-				_putchar('0' + j);
-			else
-			{
-				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
-			}
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 
 		_putchar('\n');
