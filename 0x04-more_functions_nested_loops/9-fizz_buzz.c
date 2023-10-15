0f41e97533e469e920fcd543b43ca2bbd4bc4c6c
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-15 16:18:41 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints FIZZBUZZ test
+ *
+ * Return: succes(0)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
