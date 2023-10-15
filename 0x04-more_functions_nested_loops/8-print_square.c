9d2326cfee92a622420cebb4eecb8598b6ad9acc
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-15 16:03:09 +0000

    8-print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print-square - prints square followed by newline
+ * @size: size of of square
+ *
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
