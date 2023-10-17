e155a156b0620c4c92a6508bcf094f8ee0356689
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-17 17:47:15 +0100

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int _strlen - prints string lengths
+ *
+ * @s: string being received
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}
