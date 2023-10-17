7ed36d80c89cfa375e5bfa9403e9ac0ad477df8c
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-17 06:47:45 +0100

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates pointer value to 98
+ * 
+ * @n: pointer to variable to be changed
+*/
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
