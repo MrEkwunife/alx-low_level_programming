52e290fbca33df8dbea07d62ff799e9a53477bd1
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-18 05:31:07 +0100

    betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * int _strlen - prints string lengths
- *
- * @s: string being received
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-
 int _strlen(char *s)
 {
-	int length = 0;
+	int longi = 0;
 
-	while (s[length] != '\0')
-		length++;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
 
-	return (length);
+	return (longi);
 }
+
