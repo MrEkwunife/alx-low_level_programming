63066f64ec2864fb7d6bd7bdfd1159960cb1fbe5
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-23 20:23:32 -0700

    str

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int i;
+
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+			if (*s == accept[i])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
