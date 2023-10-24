471aa13731a115e4d4e372ca0c17cc0858b22648
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-10-23 20:30:35 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strstr - finds a substring
+  * @haystack:  search string
+  * @needle: searching string
+  *
+  * Return: char
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j])
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			b++;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+		i++;
+	}
+	return ('\0');
+}
