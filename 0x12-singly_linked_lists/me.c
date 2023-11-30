5ab670888bb6ad10117d5d86a7fb4777720bc29e
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-11-30 02:30:04 -0800

    rm .swp

---
+++ 0x12-singly_linked_lists/me.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a statement to the console
+ * Return (0)
+ */
+
+int main(void)
+{
+	int num = 24;
+	int unum;
+	
+	scanf("%d", &unum);
+	
+	printf("The sum is %d\n", num + unum);
+	putchar('\n');
+}
