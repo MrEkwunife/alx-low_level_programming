b3523174217dee47bfcbb2eb34734ea45e7019b3
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-09-19 15:27:51 +0100

    ++ print_dlistint()

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -16,5 +19,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 #endif
