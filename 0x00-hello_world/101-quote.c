28ad27ffe951ebf449f89a255f6437756d3cc2bb
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-09-23 07:16:04 +0100

    fix: used write instead of printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,7 +6,6 @@
  * Return: Always 0 (Success)
  */
 int main(void) {
-  fprintf(stderr,
-          "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
   return (1);
 }
