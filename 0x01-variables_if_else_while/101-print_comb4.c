1633524b549918d572f7d00c7311c1f212d54f7b
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-09-23 14:19:34 +0100

    new: tripple digits numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints threes combo
+ *
+ * Description: This function prints all possible different
+ * combinations of three digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = i + 1; j <= 56; j++)
+		{
+			for (k = j + 1; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
