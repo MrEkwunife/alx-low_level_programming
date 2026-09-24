0045add832ffdc852f96fbe1f04b86db53d10838
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-09-24 07:04:50 +0100

    returns abolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to compute the absolute value of
+ *
+ * Return: the absolute value of the integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
